aa7ff779b817c61d97be0ff1bb4e5a421a6c601e
ernest <sacdalernest01@gmail.com>
2025-06-20 13:12:24 +1000

    add: validtors  for auth

--- src/routes/auth/index.ts
+++ src/routes/auth/index.ts
@@ -1,14 +1,16 @@
 import { Router } from "express";
 import * as authController from "../../controllers/authController";
+import { validate } from "../../middlewares/validate";
+import { loginSchema, registerSchema } from "../../validators/authValidations";
 
 const authRouter = Router();
 
 
-authRouter.post("/login", authController.login);
+authRouter.post("/login", validate(loginSchema), authController.login);
 
 
 
-authRouter.post("/register", authController.register);
+authRouter.post("/register", validate(registerSchema), authController.register);
 
 
 export default authRouter;
